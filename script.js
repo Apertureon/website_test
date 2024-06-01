f6f083897438d64e25b1594f5e21461c471e22ab
Apertureon <taianchen.chn@gmail.com>
2024-06-01 02:07:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
     const grid = document.querySelector('.gallery');
-    const msnry = new Masonry(grid, {
-        itemSelector: '.photo',
-        columnWidth: '.grid-sizer',
-        gutter: '.gutter-sizer',
-        percentPosition: true,        
-    });
+    imagesLoaded(grid, function() {
+        const msnry = new Masonry(grid, {
+            itemSelector: '.photo',
+            columnWidth: '.grid-sizer',
+            gutter: '.gutter-sizer',
+            percentPosition: true,        
+        });
 
-    imageList.forEach(image => {
-        const img = new Image();
-        img.loading = 'lazy'; // Adding lazy loading to the image
-        img.src = image.thumbnail; // Use the thumbnail for the img src
-        img.onload = function() {
-            const photoDiv = document.createElement('div');
-            photoDiv.className = 'photo';
+        imageList.forEach(image => {
+            const img = new Image();
+            img.src = image.thumbnail; // Use the thumbnail for the img src
+            img.alt = "Thumbnail image"; // Provide alternative text
+            img.onload = function() {
+                const photoDiv = document.createElement('div');
+                photoDiv.className = 'photo';
 
-            const link = document.createElement('a');
-            link.href = image.original; // Link to the original image
-            link.target = '_blank'; // Ensures the link opens in a new tab
-            link.appendChild(img); // Append the img to the link
+                const link = document.createElement('a');
+                link.href = image.original; // Link to the original image
+                link.target = '_blank'; // Ensures the link opens in a new tab
+                link.appendChild(img); // Append the img to the link
 
-            const detailsDiv = document.createElement('div'); 
-            detailsDiv.className = 'details'; 
+                const detailsDiv = document.createElement('div'); 
+                detailsDiv.className = 'details'; 
 
-            photoDiv.appendChild(link); // Append link (which contains the img) to the photoDiv
-            photoDiv.appendChild(detailsDiv); 
-            grid.appendChild(photoDiv);
+                photoDiv.appendChild(link); // Append link (which contains the img) to the photoDiv
+                photoDiv.appendChild(detailsDiv); 
+                grid.appendChild(photoDiv);
 
-            EXIF.getData(img, function() {
-                var aperture = EXIF.getTag(this, 'FNumber');
-                var shutterSpeed = EXIF.getTag(this, 'ExposureTime');
-                var iso = EXIF.getTag(this, 'ISOSpeedRatings');
+                EXIF.getData(img, function() {
+                    var aperture = EXIF.getTag(this, 'FNumber');
+                    var shutterSpeed = EXIF.getTag(this, 'ExposureTime');
+                    var iso = EXIF.getTag(this, 'ISOSpeedRatings');
 
-                if (aperture || shutterSpeed || iso) {
-                    detailsDiv.innerHTML = `f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
-                                           `${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' s' : 'N/A'} | ` +
-                                           `ISO ${iso || 'N/A'}`;
-                } else {
-                    detailsDiv.innerHTML = "No EXIF Data found.";
-                }
-            });
+                    if (aperture || shutterSpeed || iso) {
+                        detailsDiv.innerHTML = `f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
+                                            `${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' s' : 'N/A'} | ` +
+                                            `ISO ${iso || 'N/A'}`;
+                    } else {
+                        detailsDiv.innerHTML = "No EXIF Data found.";
+                    }
+                });
 
-            msnry.appended(photoDiv);
-            msnry.layout();
-        };
-    });
+                msnry.appended(photoDiv);
+                msnry.layout();
+            };
+        });
+    });    
 });
