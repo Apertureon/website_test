0392d433263d7cfd52668acf7499395b0d07e7df
Apertureon <taianchen.chn@gmail.com>
2024-06-01 13:51:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,45 +9,44 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     imagesLoaded(grid).progress(function() {
-        msnry.layout();
+
+        imageList.forEach(image => {
+            const img = new Image();
+            img.src = image.thumbnail; // Use the thumbnail for the img src
+            img.alt = "Thumbnail image"; // Provide alternative text
+            img.onload = function() {
+                const photoDiv = document.createElement('div');
+                photoDiv.className = 'photo';
+
+                const link = document.createElement('a');
+                link.href = image.original; // Link to the original image
+                link.target = '_blank'; // Ensures the link opens in a new tab
+                link.appendChild(img); // Append the img to the link
+
+                const detailsDiv = document.createElement('div'); 
+                detailsDiv.className = 'details'; 
+
+                photoDiv.appendChild(link); // Append link (which contains the img) to the photoDiv
+                photoDiv.appendChild(detailsDiv); 
+                grid.appendChild(photoDiv);
+
+                EXIF.getData(img, function() {
+                    var aperture = EXIF.getTag(this, 'FNumber');
+                    var shutterSpeed = EXIF.getTag(this, 'ExposureTime');
+                    var iso = EXIF.getTag(this, 'ISOSpeedRatings');
+
+                    if (aperture || shutterSpeed || iso) {
+                        detailsDiv.innerHTML = `f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
+                                            `${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' s' : 'N/A'} | ` +
+                                            `ISO ${iso || 'N/A'}`;
+                    } else {
+                        detailsDiv.innerHTML = "No EXIF Data found.";
+                    }
+                    msnry.layout();
+                });
+                msnry.appended(photoDiv);
+                msnry.layout();
+            };            
+        });
     });
-
-    imageList.forEach(image => {
-        const img = new Image();
-        img.src = image.thumbnail; // Use the thumbnail for the img src
-        img.alt = "Thumbnail image"; // Provide alternative text
-        img.onload = function() {
-            const photoDiv = document.createElement('div');
-            photoDiv.className = 'photo';
-
-            const link = document.createElement('a');
-            link.href = image.original; // Link to the original image
-            link.target = '_blank'; // Ensures the link opens in a new tab
-            link.appendChild(img); // Append the img to the link
-
-            const detailsDiv = document.createElement('div'); 
-            detailsDiv.className = 'details'; 
-
-            photoDiv.appendChild(link); // Append link (which contains the img) to the photoDiv
-            photoDiv.appendChild(detailsDiv); 
-            grid.appendChild(photoDiv);
-
-            EXIF.getData(img, function() {
-                var aperture = EXIF.getTag(this, 'FNumber');
-                var shutterSpeed = EXIF.getTag(this, 'ExposureTime');
-                var iso = EXIF.getTag(this, 'ISOSpeedRatings');
-
-                if (aperture || shutterSpeed || iso) {
-                    detailsDiv.innerHTML = `f/${aperture ? aperture.numerator / aperture.denominator : 'N/A'} | ` +
-                                        `${shutterSpeed ? shutterSpeed.numerator + '/' + shutterSpeed.denominator + ' s' : 'N/A'} | ` +
-                                        `ISO ${iso || 'N/A'}`;
-                } else {
-                    detailsDiv.innerHTML = "No EXIF Data found.";
-                }
-            });
-
-            msnry.appended(photoDiv);
-            /*msnry.layout();*/
-        };            
-    });        
 });
