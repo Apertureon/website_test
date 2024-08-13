ce4f66066a3eae898a0a04ac02af51484de075d2
Apertureon <taianchen.chn@gmail.com>
2024-08-13 22:29:18 +0100

    new

--- script.js
+++ script.js
@@ -82,9 +82,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     modalImg.src = photo.filePath;
                     
                     imgParameter.innerHTML =
-                        '<img src="icons/iso-filled.svg" alt="Camera" class="icon">' + (photo.aperture || 'unknown') +
-                        '<img src="icons/aperture-outline.svg" alt="Shutter Speed" class="icon">' + (photo.shutterSpeed || 'unknown') +
-                        '<img src="icons/timer-outline.svg" alt="ISO" class="icon">' + (photo.iso || 'unknown');
+                        '<img src="icons/aperture-outline.svg" alt="Camera" class="icon">' + (photo.aperture || 'unknown') +
+                        '<img src="icons/timer-outline.svg" alt="Shutter Speed" class="icon">' + (photo.shutterSpeed || 'unknown') +
+                        '<img src="icons/iso-outline.svg" alt="ISO" class="icon">' + (photo.iso || 'unknown');
                     imgLocation.textContent = `${photo.location || 'unknown'}`;
                     imgCamera.innerHTML = `${toTitleCase(photo.cameraModel || 'unknown')}`;
                     imgLens.innerHTML = `${toTitleCase(photo.lensModel || 'unknown')}`;
